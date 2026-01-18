4cb13afa074eaf8d4df20a9ecdf0c0da249ed405
danilpremgi <35040511+danilpremgi@users.noreply.github.com>
2026-01-18 19:08:13 +0000

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,9 +1,9 @@
 locals {
   dsrm_password_effective = length(trim(var.dsrm_password)) > 0 ? var.dsrm_password : var.admin_password
 
-  vnet_name        = "vnet-identity"
-  dc_subnet_name   = "snet-domaincontrollers"
-  bastion_subnet   = "AzureBastionSubnet"
+  vnet_name      = "vnet-identity"
+  dc_subnet_name = "snet-domaincontrollers"
+  bastion_subnet = "AzureBastionSubnet"
 
   dc01_name = "dc01"
   dc02_name = "dc02"
