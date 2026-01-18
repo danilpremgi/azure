3617cb3b0bfef93633e75d19c8b0b5e34812ee56
Danil Premgi <35040511+danilpremgi@users.noreply.github.com>
2026-01-18 21:27:27 +0000

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,5 +1,4 @@
 terraform {
-  required_version = ">= 1.6.0"
 
   cloud {
     organization = "danilpremgi"
@@ -12,15 +11,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 4.0.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.6.0"
-    }
+}
     time = {
       source  = "hashicorp/time"
-      version = ">= 0.10.0"
     }
   }
 }
